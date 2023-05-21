66ec5cde8dc6510d31a8cb7fad34fbb01d700eca
Kenan Klepic <kenan.klepic@gmail.com>
2023-05-21 21:05:18 +0200

    Added cache into every function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const NodeCache = require("node-cache");
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -6,6 +7,8 @@ const winston = require("winston");
 const https = require("https");
 
 const apiKey = "e06601bb8da18db5cf87be1f9498fcf0";
+
+const cache = new NodeCache();
 
 app.use( express.json() )
 
@@ -19,7 +22,7 @@ const logger = winston.createLogger({
 
 app.listen(
     PORT,
-    () => console.log('Hello')
+    () => console.log('Server Started')
 )
 
 //POST function for current weather
@@ -29,25 +32,34 @@ try{
 
     const api = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
 
+    const cached = cache.get(location);
+
 
     if (!location) {
         res.status(418).send({message: 'Type Location'})
     }
 
-    https.get(api, function(response) {
-        response.on("data", function(data) {
-            const weatherData = JSON.parse(data);
-            logger.info(weatherData);
-
-            res.send({
-                weatherData
+    if (cached) {
+        console.log("Data from cache:", cached);
+        res.json({message: "Data from cache:", cached  });
+      } else {
+        https.get(api, function(response) {
+            response.on("data", function(data) {
+                const weatherData = JSON.parse(data);
+                logger.info(weatherData);
+                cache.set(location, weatherData, 180);
+    
+                res.send({
+                    weatherData
+                });
             });
+            
         });
-        
-    });
-} catch (error){
-    return res.status(500).json({ error: "Error" });
-}
+    } 
+      }catch (error){
+        return res.status(500).json({ error: "Error" });
+    }
+ 
 });
 
 //POST function for forecast
@@ -58,7 +70,13 @@ app.post('/weather/forecast', (req, res) => {
 
     const api = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=" + apiKey;
 
-    https.get(api, function (response) {
+    const cached = cache.get(location);
+
+    if (cached) {
+        console.log("Data from cache:", cached);
+        res.json({message: "Data from cache:", cached  });
+      } else{
+        https.get(api, function (response) {
         let weatherData = "";
         response.on("data", function (data) {
           weatherData = weatherData + data;
@@ -67,11 +85,13 @@ app.post('/weather/forecast', (req, res) => {
         response.on("end", function () {
           const parsedWeatherData = JSON.parse(weatherData);
           logger.info(parsedWeatherData);
+          cache.set(location, parsedWeatherData, 180);
           res.send({
             parsedWeatherData
           });
         });
       });
+    }
     } catch(error){
         return res.status(500).json({ error: "Error" });
     }
@@ -88,6 +108,7 @@ app.post('/weather/history', async (req, res) => {
 
         const geoApi = "https://api.openweathermap.org/geo/1.0/direct?q=" + location + "&appid=" + apiKey;
 
+        const cached = cache.get(location);
         const resp = await fetch(geoApi);
         const data = await resp.json();
         const latitude = data[0].lat;
@@ -95,19 +116,26 @@ app.post('/weather/history', async (req, res) => {
 
         const api = "https://history.openweathermap.org/data/2.5/history/city?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&start="  + sDate + "&end=" + eDate;
 
-         https.get(api, function (response) {
-            let weatherData = "";
-              response.on("data", function (data) {
-                 weatherData = weatherData + data;
-                 });
-             response.on("end", function () {
-                const parsedWeatherData = JSON.parse(weatherData);
-                logger.info(parsedWeatherData);
-                res.send({
-                 weatherData
+        if (cached) {
+            console.log("Data from cache:", cached);
+            res.json({message: "Data from cache:", cached  });
+          } else{
+            https.get(api, function (response) {
+                let weatherData = "";
+                  response.on("data", function (data) {
+                     weatherData = weatherData + data;
+                     });
+                 response.on("end", function () {
+                    const parsedWeatherData = JSON.parse(weatherData);
+                    logger.info(parsedWeatherData);
+                    cache.set(location, parsedWeatherData, 180);
+                    res.send({
+                     parsedWeatherData
+             });
+            });
          });
-        });
-     });
+          }
+         
     } catch(error) {
         return res.status(500).json({ error: "Error" });
     }
